22580f023360c24ebf732f3adf2ebc1f9550303d
luisgustavo <luisgosampaio@gmail.com>
2024-06-19 14:43:17 -0300

    comeco da mecanica de rounds

--- src/boardandplayer/Player.java
+++ src/boardandplayer/Player.java
@@ -8,14 +8,18 @@ public class Player {
     //vida do jogador
     private int hp = 2;
 
+    //nome definido no main (ex: victor).
+    private String name;
+
     //deck do jogador // no maximo 30 cartas!
     private ArrayList<Card> deck = new ArrayList<Card>();
 
     //mao do jogador
     private ArrayList<Card> hand = new ArrayList<Card>();
 
-    public Player(){
+    public Player(String name){
         deck = new ArrayList<Card>();
+        this.name = name;
     }
 
     public void shuffle(ArrayList<Card> deck){
@@ -25,5 +29,29 @@ public class Player {
         for(int i = 0 ; i < 10; i++){
             int randomNumber = random.nextInt();
         }
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public void setHand(ArrayList<Card> hand) {
+        this.hand = hand;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
     }
 }
--- src/rounds/Round.java
+++ src/rounds/Round.java
@@ -1,6 +1,16 @@
 package rounds;
 
+// importando as classes 'board' e 'player'
+import boardandplayer.Board;
+import boardandplayer.Player;
+
+// importando funcionalidades
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Round {
+
+    Scanner scanner = new Scanner(System.in);
 
     private int roundNumber = 0;
 
@@ -15,7 +25,23 @@ public class Round {
         this.roundNumber = roundNumber;
     }
 
-    public void firstRound(){
+    public void roundMechanic(Board board, Player player1, Player player2){
 
+        int isPass; //flag para passar ou nao a sua vez
+
+        // loop para logica do jogo que acontecera enquanto a vida dos jogadores for diferente de algo (a definir).
+        while(){
+            System.out.println("Qual carta você deseja jogar, "+ player1.getName() +"?");
+            System.out.println(player1.getHand());
+            String nameTemp = scanner.nextLine();
+
+            for(int i = 0; i < player1.getHand().size(); i++){
+                if(nameTemp.equals(player1.getHand().get(i).getName())){
+                    if(player1.getHand().get(i).getType().equals("Artilharia")){
+                         // pensar melhor aqui
+                    }
+                }
+            }
+        }
     }
 }
